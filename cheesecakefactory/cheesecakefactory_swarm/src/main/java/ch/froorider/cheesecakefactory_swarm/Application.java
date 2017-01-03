869841fc0ffcfd8ee0384b940fb50a7b01b6e576
codeheap <froorider@gmail.com>
2017-01-03 09:24:22 +0100

    added Docker - Project

--- cheesecakefactory/cheesecakefactory_swarm/src/main/java/ch/froorider/cheesecakefactory_swarm/Application.java
+++ cheesecakefactory/cheesecakefactory_swarm/src/main/java/ch/froorider/cheesecakefactory_swarm/Application.java
@@ -2,16 +2,20 @@ package ch.froorider.cheesecakefactory_swarm;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.config.logging.Level;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
+import org.wildfly.swarm.logging.LoggingFraction;
 
 public class Application {
-
-    private Application() {
-    }
 
     public static void main(String... args) throws Exception {
 
         Swarm swarm = new Swarm();
+        Level logLevel = Level.INFO;
+        swarm.fraction(new LoggingFraction()
+                .formatter("PATTERN", "%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c] (%t) %s%e%n")
+                .consoleHandler(logLevel, "PATTERN")
+                .rootLogger(logLevel, "CONSOLE"));
         JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
         deployment.addAllDependencies();
         swarm.start().deploy(deployment);
